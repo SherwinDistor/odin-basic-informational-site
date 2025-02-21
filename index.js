e0680f491c339c08d5a1a47b11be91cdf6ed491d
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-02-21 12:55:48 -0600

    Set up port and server listen

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const fs = require("fs");
 
 // Create server
 const server = http.createServer((req, res) => {});
+
+// Set up port for connection
+const PORT = process.env.PORT || 8080;
+
+// Create server event to listen for the port
+server.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
