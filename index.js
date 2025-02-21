8a1f6fb827ec1b2e91709a51d05f3e05c80971ac
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-02-21 14:05:52 -0600

    Refactor switch statement

--- index.js
+++ index.js
@@ -3,7 +3,55 @@ const path = require("path");
 const fs = require("fs");
 
 // Create server
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  // Set header content type
+  //   res.setHeader("Content-Type", "text/html");
+
+  // Build file path for pages
+  let filePath = "./public/";
+  switch (req.url) {
+    case "/":
+      filePath += "index.html";
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      break;
+    case "/about":
+      filePath += "about.html";
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      break;
+    case "/contact-me":
+      filePath += "contact-me.html";
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      break;
+    case "/styles.css":
+      filePath += "styles.css";
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/css");
+      break;
+    default:
+      filePath += "404.html";
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/html");
+      break;
+  }
+
+  console.log(filePath);
+
+  // Read and serve file
+  fs.readFile(filePath, (err, data) => {
+    // Handle errors
+    if (err) {
+      // Handle page not found
+      console.log(err);
+      res.end();
+    } else {
+      // Successfully connected to server and found page
+      res.end(data, "utf8");
+    }
+  });
+});
 
 // Set up port for connection
 const PORT = process.env.PORT || 8080;
