99b3b057349d75022d09172ceba82f4a5d923c33
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-02-22 17:56:45 -0600

    Install express

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-const http = require("http");
-const path = require("path");
-const fs = require("fs");
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 // Create server
 const server = http.createServer((req, res) => {
-  // Build file path for pages
-  let filePath = "./public/";
-  switch (req.url) {
-    case "/":
-      filePath += "index.html";
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      break;
-    case "/about":
-      filePath += "about.html";
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      break;
-    case "/contact-me":
-      filePath += "contact-me.html";
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      break;
-    case "/styles.css":
-      filePath += "styles.css";
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/css");
-      break;
-    default:
-      filePath += "404.html";
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "text/html");
-      break;
-  }
+	// Build file path for pages
+	let filePath = './public/';
+	switch (req.url) {
+		case '/':
+			filePath += 'index.html';
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'text/html');
+			break;
+		case '/about':
+			filePath += 'about.html';
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'text/html');
+			break;
+		case '/contact-me':
+			filePath += 'contact-me.html';
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'text/html');
+			break;
+		case '/styles.css':
+			filePath += 'styles.css';
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'text/css');
+			break;
+		default:
+			filePath += '404.html';
+			res.statusCode = 404;
+			res.setHeader('Content-Type', 'text/html');
+			break;
+	}
 
-  // Read and serve file
-  fs.readFile(filePath, (err, data) => {
-    // Handle errors
-    if (err) {
-      // Handle page not found
-      console.log(err);
-      res.end();
-    } else {
-      // Successfully connected to server and found page
-      res.end(data, "utf8");
-    }
-  });
+	// Read and serve file
+	fs.readFile(filePath, (err, data) => {
+		// Handle errors
+		if (err) {
+			// Handle page not found
+			console.log(err);
+			res.end();
+		} else {
+			// Successfully connected to server and found page
+			res.end(data, 'utf8');
+		}
+	});
 });
 
 // Set up port for connection
